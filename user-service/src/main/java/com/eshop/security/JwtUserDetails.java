8e26fb5c8f52e05d1a82e47fc942e7ec4ec0cb86
maximfersko <79246330+maximfersko@users.noreply.github.com>
2024-01-23 15:17:24 +0700

    add test

--- user-service/src/main/java/com/eshop/security/JwtUserDetails.java
+++ user-service/src/main/java/com/eshop/security/JwtUserDetails.java
@@ -2,6 +2,7 @@ package com.eshop.security;
 
 import com.eshop.entity.Role;
 import lombok.Builder;
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,6 +12,7 @@ import java.util.Collections;
 
 
 @Builder
+@Getter
 public class JwtUserDetails implements UserDetails {
 	private final Long id;
 	private final String username;
